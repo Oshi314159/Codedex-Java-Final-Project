927796d8788580d5300d2d623e54e5bd21121048
Oshi <ilsamo135135@gmail.com>
2025-12-26 12:34:00 +0900

    2nd Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
-public class Main {
-    public static void main(String[] args) {
-
+class Main {
+    static void main() {
         //Create a new window
         JFrame frame = new JFrame("My first GUI");
         frame.setSize(300, 150);
